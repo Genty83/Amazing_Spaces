0c6eb471f3ff0dec3570635f3aa34268890f2e2f
Craig Gent <genty83@outlook.com>
2024-01-23 19:46:54 +0000

    Added javascript to pages

---
+++ assets/js/button_clicks.js
@@ -0,0 +1,22 @@
+// ============================== Module Notes ================================
+//
+// Module contains all button click events for the site
+//
+// ============================================================================
+
+
+// Function to redirect to brochure form
+function goToBrochure() { 
+
+    brochurePath =  "brochure.html#brochure-form"
+
+    window.location = brochurePath;
+};
+
+// Function to redirect to contact form
+function goToContactForm() { 
+
+    brochurePath =  "about.html#contact-form"
+
+    window.location = brochurePath;
+};
