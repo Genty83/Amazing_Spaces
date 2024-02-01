99e022d816e42affd01b75a2b3df1ed8ab7ce11a
Craig Gent <genty83@outlook.com>
2024-02-01 15:53:50 +0000

    Added function to open and close side nav bar

--- assets/js/button_clicks.js
+++ assets/js/button_clicks.js
@@ -4,6 +4,9 @@
 //
 // ============================================================================
 
+const navBar = document.getElementById("side-menu")
+
+
 
 // Function to redirect to brochure form
 function goToBrochure() { 
@@ -19,4 +22,19 @@ function goToContactForm() {
     brochurePath =  "about.html#contact-form"
 
     window.location = brochurePath;
+};
+
+
+// Open/Close Side menu
+function openCloseSideMenu() {
+    // Variables
+    let positionInfo = navBar.getBoundingClientRect()
+    let width = positionInfo.width
+
+    if (width > "0") {
+        navBar.style.width = "0px";
+    }
+    else {
+        navBar.style.width = "250px";
+    }
 };
